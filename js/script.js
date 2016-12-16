5907fefdfe5710f800a5f209e991f87eddcc175f
KPTeam <kp-team@hotmail.com>
2016-12-16 14:46:54 +0100

    Terminet Done

--- js/script.js
+++ js/script.js
@@ -9,13 +9,24 @@ function validateForm()
 	var surname_validation = document.getElementById("surname_validation");
         var username = document.getElementById('username');
 	var username_validation = document.getElementById("username_validation");
-        var date = document.getElementById('date');
+        var date = document.getElementById('year') +"-"+ document.getElementById('month')+"-"+ document.getElementById('day');
 	var date_validation = document.getElementById("date_validation");
-        var time = document.getElementById('time');
-	var time_validation = document.getElementById("time_validation");
         var filter= /[0-9]|\./;
         var filter2= /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
-        var filter3 =/^([0-1]?[0-9]|2[0-3]):[0-0][0-0]:[0-0][0-0]$/;
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
+        if(dd<10) {
+            dd='0'+dd
+        } 
+
+        if(mm<10) {
+            mm='0'+mm
+            } 
+
+        today = yyyy+'-'+mm+'-'+dd;
+        
 		
 	
 	if(filter.test(name.value))
@@ -54,6 +65,7 @@ function validateForm()
 		username_validation.style.display = "none";
 		username_validation.parentNode.style.backgroundColor = "transparent";
 	}
+        
         if(date.value === "" || !filter2.test(date.value))
 	{
 		valid =0;
@@ -66,20 +78,21 @@ function validateForm()
 		date_validation.style.display = "none";
 		date_validation.parentNode.style.backgroundColor = "transparent";
 	}
-        if(time.value === "" || !filter3.test(time.value))
-	{
-		valid =0;
-		time_validation.innerHTML="Shkruajeni daten sipas formatit te duhur";
-		time_validation.style.display = "block"; 	
-		time_validation.parentNode.style.backgroundColor = "FFDFDF";
-	}
-	else
-	{
-		time_validation.style.display = "none";
-		time_validation.parentNode.style.backgroundColor = "transparent";
-	}
-	
+        
+        if(date.value === today)
+        {
+           valid =0;
+		date_validation.innerHTML="Nuk lejohet data e sotme";
+		date_validation.style.display = "block"; 	
+		date_validation.parentNode.style.backgroundColor = "FFDFDF";
+        }
+        else
+        {
+            date_validation.style.display = "none";
+		date_validation.parentNode.style.backgroundColor = "transparent";
+        }
 
+   
 	
 	if(valid == 0)
             
